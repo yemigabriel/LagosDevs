fb51db4fef347c504e065711203eacea8163865e
Yemi Gabriel <yemigabriel@gmail.com>
2022-05-08 23:14:24 +0100

    Initial Commit

---
+++ LagosDevs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LagosDevs
+//
+//  Created by Yemi Gabriel on 5/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
